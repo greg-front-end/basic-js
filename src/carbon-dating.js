28b42db6f46c43183ca7e776aa0990bf617f9f4a
Greg <martinstudyweb@gmail.com>
2022-04-14 20:20:53 +0300

    feat: complete carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,6 +2,7 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const k = 0.693
 
 /**
  * Determine the age of archeological find by using
@@ -17,9 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (Number(sampleActivity) === NaN || Number(sampleActivity) <= 0 || typeof sampleActivity !== 'string' || sampleActivity.length === 0) {
+    return false
+  }
+  let time = Math.log(MODERN_ACTIVITY / Number(sampleActivity)) / (k / HALF_LIFE_PERIOD)
+  if (Math.ceil(time) > 0) return Math.ceil(time)
+  else return false
 }
 
 module.exports = {
