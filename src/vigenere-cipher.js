aa8fa5ba3c8633dd44a4dcee4338c912dc85f417
Greg <martinstudyweb@gmail.com>
2022-04-28 01:26:58 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    this.operators = {
+      '+': (a = 0, b = 0) => a + b,
+      '-': (a = 0, b = 0) => (a + this.alphabet.length) - b
+    }
+    this.direct = direct
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  mainCrypt(message, key, direct, operators) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let encrString = ''
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    for (let i = 0, idx = 0; i < message.length; i++) {
+      if (this.alphabet.includes(message[i])) {
+        let messIdx = this.alphabet.indexOf(message[i])
+        let keyIdx = this.alphabet.indexOf(key[idx % key.length])
+        encrString +=
+          this.alphabet[(operators(messIdx, keyIdx) % this.alphabet.length)]
+        idx++
+      }
+      else {
+        encrString += message[i]
+      }
+    }
+    return this.direct ? encrString : encrString.split('').reverse().join('')
+  }
+  encrypt(message, key) {
+    return this.mainCrypt(message, key, true, this.operators['+'])
+  }
+  decrypt(message, key) {
+    return this.mainCrypt(message, key, false, this.operators['-'])
   }
 }
 
